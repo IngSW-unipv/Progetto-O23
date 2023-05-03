715d610232ac55dd469a1a0a37c08ad6cc77880c
BiussUni <christian.biuso01@universtiadipavia.it>
2023-05-03 19:47:31 +0200

    aggiunta classe Dipendenti

---
+++ Progetto/src/code/Dipendenti.java
@@ -0,0 +1,36 @@
+package code;
+
+public class Dipendenti {
+	private int id_L;
+	private float Stipendio;
+
+//Costruttore
+	
+public Dipendenti(int id_L, float stipendio) {
+	this.id_L = id_L;
+	this.Stipendio = stipendio;
+	
+}
+	
+//Getter e Setter
+	public int getId_L() {
+		return id_L;
+	}
+
+	public void setId_L(int id_L) {
+		this.id_L = id_L;
+	}
+
+	public float getStipendio() {
+		return Stipendio;
+	}
+
+	public void setStipendio(float stipendio) {
+		Stipendio = stipendio;
+	}
+	
+
+	
+	
+
+}
