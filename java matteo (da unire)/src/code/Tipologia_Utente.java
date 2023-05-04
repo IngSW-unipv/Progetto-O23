096410fb8280233b1fe4992365a1e23b91ba30ab
MatteoDilo <matteo.dilorenzo01@universitadipavia.it>
2023-05-04 13:12:24 +0200

    pezzo java aggiunto, da unire al resto

---
+++ java matteo (da unire)/src/code/Tipologia_Utente.java
@@ -0,0 +1,34 @@
+package code;
+
+public class Tipologia_Utente {
+	private int Id_Tipo;
+	private String Descrizione_Utente;
+	
+//COSTRUTTORE
+	public Tipologia_Utente(int id_Tipo, String descrizione_Utente) {
+	
+		this.Id_Tipo = id_Tipo;
+		this.Descrizione_Utente = descrizione_Utente;
+	}	
+	
+	
+//GETTER E SETTER	
+	public int getId_Tipo() {
+		return Id_Tipo;
+	}
+	
+	public void setId_Tipo(int id_Tipo) {
+		Id_Tipo = id_Tipo;
+	}
+	public String getDescrizione_Utente() {
+		return Descrizione_Utente;
+	}
+	public void setDescrizione_Utente(String descrizione_Utente) {
+		Descrizione_Utente = descrizione_Utente;
+	}
+}
+
+
+//METHODS
+
+
