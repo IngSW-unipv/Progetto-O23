f8d85ca61dc88f4ee008cccc37ee1ed88a5760fa
MatteoDilo <matteo.dilorenzo01@universitadipavia.it>
2023-05-10 13:37:24 +0200

    mi piace il progetto

--- Progetto/src/luoghi/Città.java
+++ Progetto/src/luoghi/Città.java
@@ -1,22 +1,22 @@
 package luoghi;
 
-public class Citt‡{
-	private int Id_Citt‡;
+public class Citt√†{
+	private int Id_Citt√†;
 	private String Nome_C;
 	
 	//COSTRUTTORE
-	public Citt‡(int id_Citt‡, String nome_C) {
-		this.Id_Citt‡ = id_Citt‡;
+	public Citt√†(int id_Citt√†, String nome_C) {
+		this.Id_Citt√† = id_Citt√†;
 		this.Nome_C = nome_C;
 	}
 
 	
 //GETTER & SETTER
-	public void setId_Citt‡(int id_Citt‡) {
-		Id_Citt‡ = id_Citt‡;
+	public void setId_Citt√†(int id_Citt√†) {
+		Id_Citt√† = id_Citt√†;
 	}
-	public int getId_Citt‡() {
-		return Id_Citt‡;
+	public int getId_Citt√†() {
+		return Id_Citt√†;
 	}
 	
 	public String getNome_C() {
--- Progetto/src/test/Test.java
+++ Progetto/src/test/Test.java
@@ -1,14 +1,17 @@
 package test;
 
-import interfacciaGrafica.Gui;
+import interfacciaGrafica.Benvenuto_Form;
+import interfacciaGrafica_User.Login_Success_GUI;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	
-		
+		Benvenuto_Form Benvenuto_Form = new Benvenuto_Form();
+		Benvenuto_Form.setVisible(true);
+
+		Login_Success_GUI Login_Success_GUI = new Login_Success_GUI();
+		Login_Success_GUI.setVisible(true);
 
 	}
 
