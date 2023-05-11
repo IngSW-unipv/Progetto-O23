acb76ab607ebe64d8d618a31a198b9727d684451
BiussUni <christian.biuso01@universtiadipavia.it>
2023-05-11 12:31:37 +0200

    prova connesione database

---
+++ Progetto/src/dao/DBConnessione.java
@@ -0,0 +1,37 @@
+package dao;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnessione {
+	
+		private String url = "jdbc:mysql://localhost:3306/hotel";
+	    private String user = "root";
+	    private String password = "Biuss90!";
+	    private String driver="com.mysql.jdbc.Driver";
+	
+	
+	    public void connessione() {
+	    	
+	    	try {
+	    		Class.forName(driver);
+	    		Connection con = DriverManager.getConnection(url, user, password);
+	    		
+	    		System.out.println("connesione avvenuta");
+	    	
+	    		
+	    } catch (ClassNotFoundException e) {
+				Logger.getLogger(DBConnessione.class.getName()).log(Level.SEVERE,null,e);
+		} catch (SQLException e) {
+				Logger.getLogger(DBConnessione.class.getName()).log(Level.SEVERE,null,e);
+			}
+	        
+
+	    }
+	    
+	
+}
--- Progetto/src/test/Test.java
+++ Progetto/src/test/Test.java
@@ -3,6 +3,7 @@ package test;
 
 import java.sql.SQLException;
 
+import dao.DBConnessione;
 import dao.User_Dao;
 import interfacciaGrafica.Benvenuto_Form;
 import interfacciaGrafica_User.Login_Success_GUI;
@@ -11,8 +12,12 @@ public class Test {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-		Benvenuto_Form Benvenuto_Form = new Benvenuto_Form();
-		Benvenuto_Form.setVisible(true);
+		/*Benvenuto_Form Benvenuto_Form = new Benvenuto_Form();
+		Benvenuto_Form.setVisible(true);*/
+		
+		DBConnessione d= new DBConnessione();
+		
+		d.connessione();
 	}
 
 }
