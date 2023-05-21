3884012586db0d2cb8e796d5c068817bbe427923
BiussUni <christian.biuso01@universtiadipavia.it>
2023-05-21 11:39:58 +0200

    aggiunte modifice tabelle db

---
+++ DataBase/hotel Db/hotel_turni_lavoro.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: hotel
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `turni_lavoro`
+--
+
+DROP TABLE IF EXISTS `turni_lavoro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turni_lavoro` (
+  `ID_L` int NOT NULL,
+  `GIORNO` date NOT NULL,
+  `NUM_CT` int NOT NULL,
+  `ORA_INIZIO` time NOT NULL,
+  `ORA_FINE` time NOT NULL,
+  PRIMARY KEY (`GIORNO`),
+  KEY `ID_idx` (`ID_L`),
+  KEY `NUM_idx` (`NUM_CT`),
+  CONSTRAINT `ID_TR` FOREIGN KEY (`ID_L`) REFERENCES `dipendente` (`ID_L`),
+  CONSTRAINT `NUM_T` FOREIGN KEY (`NUM_CT`) REFERENCES `camera` (`NUMERO`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turni_lavoro`
+--
+
+LOCK TABLES `turni_lavoro` WRITE;
+/*!40000 ALTER TABLE `turni_lavoro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `turni_lavoro` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-21 11:26:57
